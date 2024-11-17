5be6239491c506d6b25364cb33a209ae9b67ab60
bobelmasry <alyhassan2254@gmail.com>
2024-11-17 18:08:17 +0200

    changed msjson path

--- makep4/combine_json.py
+++ makep4/combine_json.py
@@ -1,32 +1,59 @@
 import json
 from os.path import exists
 
-subjectcode=9702
-subjectname="physics"
-jsondirectory=r"D:\python_projects\teachmegcse\python_files\makep4\phy_db_final_p4.json"
-MSjsonDirectory=r"D:\python_projects\teachmegcse\python_files\classified\testp4.json"
-QuestionJsonDirectory=r"D:\python_projects\teachmegcse\json_files\phy_db_p4.json"
+subjectcode = 9702
+subjectname = "physics"
+jsondirectory = r"D:\python_projects\teachmegcse\python_files\makep4\phy_db_final_p4.json"
+MSjsonDirectory = r"D:\python_projects\teachmegcse\json_files\phy_db_ms_p4.json"
+QuestionJsonDirectory = r"D:\python_projects\teachmegcse\json_files\phy_db_p4.json"
 
-def combineJSON(MSjson,QPjson,jsonDirectory):
+def normalize_paper_code(code):
+    """Normalize paper code by removing ms/qp and converting to lowercase."""
+    return code.lower().replace("_ms_", "_").replace("_qp_", "_")
+
+def combineJSON(MSjson, QPjson, jsonDirectory):
+    print("Starting JSON combination process...")
     totalData = []
-    MSData = json.load(open(MSjson))
-    QPData = json.load(open(QPjson))
-    if exists(jsonDirectory):
-        with open(jsonDirectory, 'r') as f:
-            totalData = json.load(f)
-    for QPindex in range(len(QPData)):
-        MSindex = 0
-        while MSindex < len(MSData):
-            if MSData[MSindex]["questionNumber"] == QPData[QPindex]["questionNum"] and MSData[MSindex]["paperCode"].replace("ms","qp") == QPData[QPindex]["pdfName"]:
-                totalData.append({
-                    "questionName": QPData[QPindex]["questionName"],
-                    "MSName": MSData[MSindex]["fileName"],
-                    "questionNumber": MSData[MSindex]["questionNumber"],
-                    "pdfName": QPData[QPindex]["pdfName"]
-                })
-                break
-            MSindex += 1
+    
+    # Load the JSON files
+    print("Loading JSON files...")
+    with open(MSjson, 'r') as f:
+        MSData = json.load(f)
+    with open(QPjson, 'r') as f:
+        QPData = json.load(f)
+    
+    print(f"Found {len(MSData)} mark scheme entries")
+    print(f"Found {len(QPData)} question paper entries")
+    
+    # Create a lookup dictionary for mark scheme data
+    ms_lookup = {}
+    for ms_entry in MSData:
+        key = (ms_entry["questionNumber"], normalize_paper_code(ms_entry["paperCode"]))
+        ms_lookup[key] = ms_entry
+    
+    # Match questions with mark schemes
+    matches_found = 0
+    for qp_entry in QPData:
+        qp_code = normalize_paper_code(qp_entry["pdfName"])
+        key = (qp_entry["questionNum"], qp_code)
+        
+        if key in ms_lookup:
+            matches_found += 1
+            ms_entry = ms_lookup[key]
+            totalData.append({
+                "questionName": qp_entry["questionName"],
+                "MSName": ms_entry["fileName"],
+                "questionNumber": ms_entry["questionNumber"],
+                "pdfName": qp_entry["pdfName"]
+            })
+    
+    print(f"Found {matches_found} matching pairs")
+    
+    # Save the combined data
+    print("Saving combined data...")
     with open(jsonDirectory, 'w') as totalJson:
         json.dump(totalData, totalJson, indent=1)
+    
+    print("Combination complete!")
 
-combineJSON(MSjsonDirectory,QuestionJsonDirectory,jsondirectory)
+combineJSON(MSjsonDirectory, QuestionJsonDirectory, jsondirectory)
