b05f99e291e779b908baf99e35ce3377057d8c2a
Aly Youssef <103034201+bobelmasry@users.noreply.github.com>
2025-05-14 10:57:04 +0300

    .

--- pathConst.py
+++ pathConst.py
@@ -2,6 +2,6 @@ BASE_PATH = r"D:\python_projects\teachmegcse"
 
 POPPLER_PATH = r"D:\python_projects\poppler-23.05.0\Library\bin"
 
-tesseractPath = r"D:\python_projects\Tesseract-OCR\tesseract.exe"
+TESSERACT_PATH = r"D:\python_projects\Tesseract-OCR\tesseract.exe"
 
 PDF_PATH = rf"D:/python_projects/teachmegcse/full_pdfs"
