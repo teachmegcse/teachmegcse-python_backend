0516d383c6ccbc838049666fd210e423f98130c8
Aly Youssef <103034201+bobelmasry@users.noreply.github.com>
2025-06-15 14:12:10 +0300

    added long vision model and removed pathConst

--- pathConst.py
+++
@@ -1,7 +0,0 @@
-BASE_PATH = r"D:\python_projects\teachmegcse"
-
-POPPLER_PATH = r"D:\python_projects\poppler-23.05.0\Library\bin"
-
-TESSERACT_PATH = r"D:\python_projects\Tesseract-OCR\tesseract.exe"
-
-PDF_PATH = rf"D:/python_projects/teachmegcse/full_pdfs"
